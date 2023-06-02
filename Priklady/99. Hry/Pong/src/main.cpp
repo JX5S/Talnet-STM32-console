4ea067a3b0a0814cf494ce6cafbef64d0714cc4f
JX5S <40174559+JX5S@users.noreply.github.com>
2023-06-02 21:44:33 +0200

    Update pong
    Přidány komentáře

--- Priklady/99. Hry/Pong/src/main.cpp
+++ Priklady/99. Hry/Pong/src/main.cpp
@@ -1,90 +1,78 @@
-/*
- * Pong
- * Original Code from https://github.com/rparrett/pongclock
- *
- */
-
-#define BLACK 0x0000
-#define WHITE 0xFFFF
-#define GREY  0x5AEB
-
-#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <TFT_eSPI.h>
 #include <SPI.h>
 #include "console.h"
 
-TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite frame_buffer = TFT_eSprite(&tft);
 
+// velikost displeje
 int16_t h = 128;
 int16_t w = 160;
+
 int16_t pad_width = 35;
 int16_t pad_height = 6;
-int pad_x = 50;
+int pad_x = w/2;
 
 int16_t ball_r = 2;
 float ball_x = w/2-ball_r;
 float ball_y = 20.0;
-//float ball_dx = 2.5;
-//float ball_dy = 2.5;
 float ball_dx = 2.5;
 float ball_dy = 2.5;
 
 int skore = 0;
 int highscore = 0;
 
-TFT_eSprite frame_buffer = TFT_eSprite(&tft);
-
+// Takto se řekne kompilátoru, že tyto příkazy existují, dole jsou pak nadefinovány
 void paddle();
 void ball();
 
 void setup(void) {
-  pinMode(PB0, OUTPUT);
+  // podsvícení
   pinMode(PB3, OUTPUT);
   digitalWrite(PB3, HIGH);
-  randomSeed(analogRead(0)*analogRead(1));
-   
+
   tft.init();
-
   tft.setRotation(3);
-
-  tft.fillScreen(BLACK);
+  tft.fillScreen(TFT_BLACK);
   
   frame_buffer.createSprite(w, h);
+  frame_buffer.setTextColor(TFT_WHITE, TFT_BLACK);
+  frame_buffer.setTextDatum(MC_DATUM); // "datum" = kde je nulový bod textu; 'MC_DATUM' = middle center
 
-  frame_buffer.setTextColor(WHITE, BLACK);
-  frame_buffer.setTextDatum(MC_DATUM);
-
-  for (int i = 0; i <= 60; i++){
-    frame_buffer.fillRect(0,0,160,128,BLACK);
+  // titulní obrazovka, na přibližně dvě sekundy
+  for (int i = 0; i <= 60; i++){ // toto se stane 60x. Viz Arduino Reference
+    frame_buffer.fillRect(0,0,160,128,TFT_BLACK);
     frame_buffer.drawString("Pong", w/2, h/2);
     paddle();
     frame_buffer.fillCircle(int(ball_x), int(ball_y), ball_r, TFT_YELLOW);
     frame_buffer.pushSprite(0,0);
     delay(33);
   }
-  
 }
 
 void loop() {
-  frame_buffer.fillRect(0,0,160,128,BLACK);
+  frame_buffer.fillRect(0,0,160,128,TFT_BLACK);
   paddle();
   ball();
   frame_buffer.setTextDatum(TR_DATUM);
   frame_buffer.drawString(String(skore) + "/" + String(highscore), 160, 0);
   frame_buffer.pushSprite(0,0);
-//  delay(33);
-  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+
+  // delay(33);
+  // delay není potřeba - ono to běží svou rychlostí, pushSprite trvá relativně dlouho
 }
 
 void paddle() {
   pad_x = analogRead(Console_Joy_X);
   pad_x = map(pad_x, 280, 750, 0, w-pad_width);
-  frame_buffer.fillRect(pad_x, h-pad_height, pad_width, pad_height, WHITE);
+  frame_buffer.fillRect(pad_x, h-pad_height, pad_width, pad_height, TFT_WHITE);
 }
 
 void ball() {
   ball_x += ball_dx;
   ball_y += ball_dy;
-  if(ball_x > w-ball_r || ball_x < ball_r){
+  
+  if(ball_x > w - ball_r || ball_x < ball_r){
     ball_dx *= -1;
     ball_x += ball_dx;
   }
@@ -92,8 +80,9 @@ void ball() {
     ball_dy *= -1;
     ball_y += ball_dy;
   }
-  if(ball_y>h-ball_r-pad_height){
-    if(abs(ball_x-(pad_x+pad_width/2)) <= pad_width/2+2){
+  
+  if(ball_y > h - ball_r - pad_height){
+    if(ball_x >= pad_x - 1 && ball_x <= pad_x + pad_width + 1){
       ball_dy *= -1;
       ball_y += ball_dy;
       skore += 1;
